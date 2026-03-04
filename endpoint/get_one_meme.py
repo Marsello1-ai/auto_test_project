829316059e39f30ecb1b388361dc673db23b4973
Khusnutdinov Marsel <marsepta806@gmail.com>
2026-03-04 09:52:31 +0300

    Add allure

--- endpoint/get_one_meme.py
+++ endpoint/get_one_meme.py
@@ -1,16 +1,22 @@
 import requests
 from endpoint.main_endpoint import MainEndpoint
+import allure
 
 
 class MethodGetOneMeme(MainEndpoint):
     meme_id = None
 
+    @allure.title('Показываем 1 мем')
     def get_one_meme(self, meme_id, token_id):
-        headers = {'Content-Type': 'application/json', 'Authorization': token_id}
+        with allure.step('Создаем мем и отдаем его meme_id'):
+            headers = {'Content-Type': 'application/json', 'Authorization': token_id}
         self.meme_id = meme_id
-        self.response = requests.get(f'{self.url}meme/{meme_id}', headers=headers)
 
-        return self.response
+        with allure.step('Дергаем запрос на просмотр созданного мема'):
+            self.response = requests.get(f'{self.url}meme/{meme_id}', headers=headers)
+
+        with allure.step('Отдаем тело ответа и удаляем мем'):
+            return self.response
 
     def get_one_meme_negative_meme_id(self, token_id):
         headers = {'Content-Type': 'application/json', 'Authorization': token_id}
